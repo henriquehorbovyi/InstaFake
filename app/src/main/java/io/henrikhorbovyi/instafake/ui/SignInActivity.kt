85ace702dccbf888f082f3ee289150e97efd9e71
Lucas <lucaspedrosa11@hotmail.com>
2020-09-16 17:12:10 -0300

    Created enable/disable function for Sign In Button

--- app/src/main/java/io/henrikhorbovyi/instafake/ui/SignInActivity.kt
+++ app/src/main/java/io/henrikhorbovyi/instafake/ui/SignInActivity.kt
@@ -2,11 +2,34 @@ package io.henrikhorbovyi.instafake.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
 import io.henrikhorbovyi.instafake.R
+import io.henrikhorbovyi.instafake.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    private var isSignInButtonEnabled: Boolean = false
+    private val binding: ActivitySignInBinding by lazy {
+        ActivitySignInBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(binding.root)
+
+        binding.signInInputUsername.doOnTextChanged { text, _, _, _ ->
+            binding.signInButton.isEnabled =
+            checkIsNullOrBlank(text.toString(), binding.signInInputPassword.text.toString())
+        }
+
+        binding.signInInputPassword.doOnTextChanged { text, _, _, _ ->
+            binding.signInButton.isEnabled =
+            checkIsNullOrBlank(text.toString(), binding.signInInputUsername.text.toString())
+        }
+
+    }
+
+    fun checkIsNullOrBlank(username: String, password: String): Boolean {
+            return !username.isNullOrBlank() && !password.isNullOrBlank()
     }
 }
