00ca88a11bc45e2f44553c14c6dce4f8b424d317
Lucas <lucaspedrosa11@hotmail.com>
2020-09-16 14:31:10 -0300

    Created InstaFake Log in screen

---
+++ app/src/main/java/io/henrikhorbovyi/instafake/ui/SignInActivity.kt
@@ -0,0 +1,12 @@
+package io.henrikhorbovyi.instafake.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.henrikhorbovyi.instafake.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
